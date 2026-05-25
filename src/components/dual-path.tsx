c9ecb804d0252c959c6c017a751ea1c542f99114
TiroMatlapeng <matlapengmatlapeng@gmail.com>
2026-05-25 19:00:30 +0200

    fix: add use client directive to components with event handlers

--- src/components/dual-path.tsx
+++ src/components/dual-path.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { SectionReveal } from "./section-reveal";
 
--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SectionReveal } from "./section-reveal";
 
 const TECH_PILLS = [
